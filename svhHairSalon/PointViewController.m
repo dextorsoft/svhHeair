ab716ad91d41ee6520028058910e2905aec47299
jinsu lee <kmkby@naver.com>
2015-11-19 10:31:41 +0900

    세팅 화면 포인트뷰 파일 2ea

--- svhHairSalon/PointViewController.m
+++ svhHairSalon/PointViewController.m
@@ -8,7 +8,10 @@
 
 #import "PointViewController.h"
 
-@interface PointViewController ()
+@interface PointViewController ()<UITableViewDataSource,UITableViewDelegate>{
+    NSMutableArray *pointData;
+    NSArray *pointArr;
+}
 
 @end
 
@@ -17,11 +20,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.title = @"Point";
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    [self pointCallData];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)pointCallData{
+    ForToolClass *forToolClass = [[ForToolClass alloc] init];
+    pointData = [[NSMutableArray alloc] init];
+    
+    /****
+     myNum, myDevice 는 sqlite 와 환경변수에서 가져와야함... 차후 수정
+     ****/
+    
+    NSString *myNum = @"1";
+    NSString *myDevice = @"12";
+    
+    NSString *pointURL = [NSString stringWithFormat:@"http://%s/m_point_list.php?my_num=%@&my_device=%@", KN_HOST_NAME, myNum ,myDevice];
+    NSString *pointParse = [forToolClass GetHTMLString:pointURL encoding:KN_SERVER_LANG];
+    pointParse = [pointParse stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    pointArr = [pointParse componentsSeparatedByString:@"<br>"];
+    NSLog(@"pointArr == %@", pointArr);
+    
+    //for()
 }
 
 /*
